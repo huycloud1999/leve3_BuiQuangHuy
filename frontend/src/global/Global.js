faa55d9a088144d164b4e907025120423864827f
SieuBuong <huycloud1999@gmail.com>
2023-08-27 19:48:49 +0700

    done

--- frontend/src/global/Global.js
+++ frontend/src/global/Global.js
@@ -1,2 +1,2 @@
-export const url = "http://localhost:8888";
-// export const url = "https://backend-5a9v.onrender.com";
+// export const url = "http://localhost:8888";
+export const url = "https://backend-5a9v.onrender.com";
